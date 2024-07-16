1f35f6a75a725ac08ca6e66a582ef931fe8d0a2a
SysWhiteDev <syswhite.commercial@gmail.com>
2024-07-16 03:54:07 +0200

    Updated auth screen ui, added notice banner on top, deployed the app

--- app/(auth)/auth/signin/page.tsx
+++ app/(auth)/auth/signin/page.tsx
@@ -4,12 +4,13 @@ import { SiGithub } from "react-icons/si";
 
 export default async function SignInPage() {
   return (
-    <div className="h-dvh grid-pattern w-dvw !bg-slate-400 flex justify-center items-center">
+    <div className="h-dvh grid-pattern w-dvw !bg-slate-300 flex justify-center items-center">
       <div className="bg-slate-200 border-2 shadow shadow-slate-500 border-slate-300 w-[350px] p-4 rounded-2xl">
-        <div className="mb-10">
-          <p className="text-3xl font-bold">Hello!</p>
-          <p className="text-md opacity-90">Please authenticate</p>
+        <div className="">
+          <p className="text-2xl font-bold text-center">TimeManager</p>
+          <p className="text-sm opacity-90 text-center">Welcome back!</p>
         </div>
+        <div className="h-[2px] rounded-full my-5  bg-slate-300 " />
         <form
           action={async () => {
             "use server";
--- app/api/dash/goal/route.ts
+++ app/api/dash/goal/route.ts
@@ -14,5 +14,6 @@ export const GET = auth(async function GET(req, { params }) {
             createdAt: true,
         }
     });
+    await prisma.$disconnect();
     return Response.json(goal, { status: 200 });
 });
