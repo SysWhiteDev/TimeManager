04c4fa4b424122847fcb104b6add9095f7e99f16
SysWhiteDev <syswhite.commercial@gmail.com>
2024-07-17 02:20:28 +0200

    Added auto redirect for root and made lb page server side

--- app/(dash)/dash/lb/page.tsx
+++ app/(dash)/dash/lb/page.tsx
@@ -1,32 +1,39 @@
+"use server";
+import Link from "next/link";
 import React from "react";
 
-function LbSelector({ children, active }: any): React.JSX.Element {
-  return (
-    <button
-      className={
-        active || false
-          ? "text-neutral-800 dark:text-neutral-300 dark:bg-neutral-900 bg-neutral-200 transition-all font-semibold rounded-md py-1.5"
-          : "text-neutral-800 dark:text-neutral-300 hover:bg-neutral-200 dark:hover:bg-neutral-900 dark:hover:bg-opacity-35 hover:bg-opacity-50 transition-all rounded-md py-1.5"
-      }
-    >
-      {children}
-    </button>
-  );
-}
+export default async function Page({ searchParams }: any) {
+  const timespan = searchParams.timespan || "week";
 
-export default function Page(): React.JSX.Element {
+  function LbSelector({ children, active, targetTimespan }: any): React.JSX.Element {
+    return (
+      <Link
+        href={`?timespan=${targetTimespan}`}
+        className={
+          timespan === targetTimespan
+            ? "text-neutral-800 text-center dark:text-neutral-300 dark:bg-neutral-900 bg-neutral-200 transition-all font-semibold rounded-md py-1.5"
+            : "text-neutral-800 text-center dark:text-neutral-300 hover:bg-neutral-200 dark:hover:bg-neutral-900 dark:hover:bg-opacity-35 hover:bg-opacity-50 transition-all rounded-md py-1.5"
+        }
+      >
+        {children}
+      </Link>
+    );
+  }
+
   return (
     <div className="max-w-6xl px-4 sm:px-0 mx-auto pt-[10dvh] pb-8">
       <p className="text-6xl font-semibold pt-2">Leaderboard</p>
       <div className="bg-neutral-100 dark:bg-neutral-800 dark:border-neutral-700 grid grid-cols-3 gap-1.5 border border-white p-2 rounded-xl mt-4">
-        <LbSelector active>This week</LbSelector>
-        <LbSelector>This month</LbSelector>
-        <LbSelector>Lifetime</LbSelector>
+        <LbSelector targetTimespan={"week"} active>
+          This week
+        </LbSelector>
+        <LbSelector targetTimespan={"month"}>This month</LbSelector>
+        <LbSelector targetTimespan={"all"}>Lifetime</LbSelector>
       </div>
       <div className="bg-neutral-100 dark:bg-neutral-800 dark:border-neutral-700 border border-white p-2 rounded-xl mt-4">
         <div className="flex text-sm  uppercase text-neutral-700 dark:text-neutral-300 dark:bg-neutral-900 items-center justify-between p-2 px-3 bg-neutral-200 rounded-md font-semibold">
           <div className="flex items-center">
-            <div className="min-w-[46px] flex-shrink-0">#</div>
+            <div className="min-w-[46px] flex-shrink-0 mr-4">#</div>
             <span>User</span>
           </div>
           <span>Completion Points</span>
@@ -37,7 +44,7 @@ export default function Page(): React.JSX.Element {
             className="flex items-center justify-between p-2.5 px-3"
           >
             <div className="flex items-center">
-              <div className="min-w-[46px] flex-shrink-0">{index + 1}</div>
+              <div className="min-w-[46px] flex-shrink-0 mr-4">{index + 1}</div>
               <span>SysWhite</span>
             </div>
             <span>1920</span>
@@ -45,10 +52,10 @@ export default function Page(): React.JSX.Element {
         ))}
         <div className="flex font-bold text-sm text-neutral-700 dark:text-neutral-200 items-center justify-between border border-green-300 dark:border-green-700 dark:bg-green-800 p-2 px-3 bg-green-100 rounded-md">
           <div className="flex items-center">
-            <div className="min-w-[46px] flex-shrink-0">51</div>
+            <div className="min-w-[46px] flex-shrink-0 mr-4">251</div>
             <span>User</span>
           </div>
-          <span>918239128390128390120931290</span>
+          <span>1000</span>
         </div>
       </div>
     </div>
--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,25 @@
-import { auth } from "@/auth"
+import { auth } from "@/auth";
 
-const unauthrizedRoutes = ["/auth/signin"]
+const unauthrizedRoutes = ["/auth/signin"];
 
 export default auth((req) => {
-    if (!unauthrizedRoutes.includes(req.nextUrl.pathname) && !req.auth) {
-        const newUrl = new URL("/auth/signin", req.nextUrl.origin)
-        return Response.redirect(newUrl)
-    }
-    if (req.nextUrl.pathname.startsWith("/auth") && req.auth) {
-        const newUrl = new URL("/dash", req.nextUrl.origin)
-        return Response.redirect(newUrl);
-    }
-})
+  if (req.nextUrl.pathname === "/") {
+    const newUrl = new URL("/dash", req.nextUrl.origin)
+    return Response.redirect(newUrl);
+  }
+
+  if (!unauthrizedRoutes.includes(req.nextUrl.pathname) && !req.auth) {
+    const newUrl = new URL("/auth/signin", req.nextUrl.origin);
+    return Response.redirect(newUrl);
+  }
+  if (req.nextUrl.pathname.startsWith("/auth") && req.auth) {
+    const newUrl = new URL("/dash", req.nextUrl.origin);
+    return Response.redirect(newUrl);
+  }
+});
 
 export const config = {
-    matcher: [
-        '/((?!_next/static|_next/image|favicon.ico|api|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-    ],
-}
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|api|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
